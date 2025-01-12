1565ba93804f12b5d6e73e8a9efa929def2f976a
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2025-01-12 20:44:41 +0200

    added two threads

--- InternshipGame2/main.cpp
+++ InternshipGame2/main.cpp
@@ -1,13 +1,33 @@
 
 #include <iostream>
+#include <thread>
 #include "WindowRenderer.h"
 
+std::atomic<bool> isRunning{ true };
 
-int main()
-{
+void gameLogicThread(GameHandler* gameHandler) {
+    while (isRunning) {
+        // Тут можна викликати метод оновлення логіки, якщо він є
+        // Наприклад:
+        // gameHandler->UpdateGameLogic();
+        std::this_thread::sleep_for(std::chrono::milliseconds(16)); // ~60 FPS
+    }
+}
+
+int main() {
     std::cout << "Hello World!\n";
-    GameEventManager eventManager;
     GameHandler gameHandler;
+
+    // Запускаємо потік логіки
+    std::thread logicThread(gameLogicThread, &gameHandler);
+
+    // Запускаємо рендеринг у головному потоці
     WindowRenderer renderer(1400, 900, &gameHandler);
     renderer.RunTheGame();
+
+    // Після закриття вікна завершимо потік логіки
+    isRunning = false;
+    logicThread.join();
+
+    return 0;
 }
